08fbc54e5ef7d6c5ee037aabcc3a0b3e281299e8
Dypso <dypsok@gmail.com>
2023-09-01 19:32:43 +0200

    Add comments and documentation to ZeroAllocService

--- ZeroAllocationWebAPI/Services/ZeroAllocService.cs
+++ ZeroAllocationWebAPI/Services/ZeroAllocService.cs
@@ -3,15 +3,19 @@ using ZeroAllocationWebAPI.Models;
 using System.Span;
 using Microsoft.Extensions.ObjectPooling;
 
+// This service implements zero allocation techniques
 namespace ZeroAllocationWebAPI.Services
 {
     public class ZeroAllocService
     {
+        // Processes a complex object with zero allocation
         public void ProcessComplexObject(ComplexObject object)
         {
-            Span<byte> buffer = stackalloc.alloc(1024);
+            // Allocate memory on the stack
+            Span<byte> buffer = stackallo.alloc(1024);
+            // Copy data from the object to the buffer
             object.Name.AsSpan().CopyToSpan(buffer);
-            // do more processing here
+            // Additional processing can be done here
         }
     }
 }
