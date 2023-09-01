101c151c5986ef68100385d74a118115d4d6167d
Dypso <dypsok@gmail.com>
2023-09-01 19:20:06 +0200

    Add ZeroAllocService for handling zero allocation logic

---
+++ ZeroAllocationWebAPI/Services/ZeroAllocService.cs
@@ -0,0 +1,14 @@
+using System;
+using ZeroAllocationWebAPI.Models;
+using System.Text;
+
+namespace ZeroAllocationWebIPI.Services
+{
+    public class ZeroAllcontroller : Controller
+    {
+        public void ProcessComplexObject(ComplexObject object)
+        {
+            // To be implemented
+        }
+    }
+}
