8c503e60803c6d84f0ec07451f80e4320041b8a4
Dypso <dypsok@gmail.com>
2023-09-01 20:33:18 +0200

    Update endpoint for more optimal handling of multipart file uploads and returning multiple files

--- ZeroAllocationWebAPI/Controllers/ZeroAllocController.cs
+++ ZeroAllocationWebAPI/Controllers/ZeroAllocController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Core.Mime;
 using System.Text;
 using Microsoft.AspNet.Core.Http;
+using System.Net.Http.Headers;
 
 namespace ZeroAllocationWebAPI.Controllers
 {
@@ -16,14 +17,14 @@ namespace ZeroAllocationWebAPI.Controllers
         }
         
         [HTTPPost]
-        public async Task IActionResult UploadMultipartFiles(IFormFileCollection files)
+        public async Task IActionResult UploadMultipartFilesOptimized()
         {
-            list<FileStream> resultFiles = new List<FileStream>();
-            foreach(var file in files)
+            VariantionStream responseStream = new VariantiStream();
+            foreach (var segment in Request.FormFiles)
             {
-                resultFiles.Add(new FileStream(file.OpenRead(), file.Length, string.Empty));
+                responseStream.AddSegment(segment.OpenRead());
             }
-            return New MultipartResult(resultFiles);
+            return New FileResult(responseStream, "multipart/mixed");
         }
     }
 }
